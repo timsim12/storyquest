b779f1aa13ea0c362450cdcaf04f7319d804ef18
baibrea <breannacbai@gmail.com>
2024-09-28 15:57:29 -0400

    Fixed images

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div className="flex bg-[#5087D0] font-fredoka items-center px-[10%] py-[12px] text-yellow-500 tracking-widest">
             <Link to="../" className="font-bold text-[36px] drop-shadow-lg flex">StoryQuest
-                <img src="star.png" className="w-[50px] drop-shadow-lg"></img>
+                <img src="images/star.png" className="w-[50px] drop-shadow-lg"></img>
             </Link>
             <Link to="../Login" className="text-[20px] px-[34px] py-[10px] text-black bg-yellow-500 border-yellow-600 border-[4px] hover:bg-yellow-600 transition-all duration-200 rounded-[16px] drop-shadow-lg ml-auto">Login</Link>
         </div>
--- src/components/StarImage.js
+++ src/components/StarImage.js
@@ -1,6 +1,6 @@
 function StarImage() {
     return (
-        <img src="star_1.png" className="w-[34px]"></img>
+        <img src="/images/star_1.png" className="w-[34px]"></img>
     )
 }
 
