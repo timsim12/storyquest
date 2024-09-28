06d075d9a7e6a8a566042017458b85231b805c3d
Lexline Johnson <lexlinejj27@gmail.com>
2024-09-28 16:15:25 -0400

    Aesthetics ✨

--- src/pages/BookView.js
+++ src/pages/BookView.js
@@ -9,15 +9,17 @@ function BookView({title, text}) {
     }
     
     return (
-        <>
+        <div className="font-fredoka tracking-widest">
             <Header />
-            <h1>{title}</h1>
-            <body>
-                <div className="w-[50%]">
-                    <body onMouseUp={selectedText}>{text}</body>
-                </div>
-            </body>
-        </>
+            <div className="bg-white mt-[20px] mx-[10%] rounded-[20px] p-[20px] px-[60px]">
+                <body>
+                <h1 className="text-[40px] mb-[34px] font-bold text-left text-[#5087D0] drop-shadow-lg">{title}</h1>
+                    <div className="w-[50%]">
+                        <body onMouseUp={selectedText}>{text}</body>
+                    </div>
+                </body>
+            </div>
+        </div>
     )
 }
 
