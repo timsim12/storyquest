875b0a6c3b9fc707fb064d9f6f3a35b879202eba
baibrea <breannacbai@gmail.com>
2024-09-28 10:25:11 -0400

    Added more CSS to Login and Register pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div className="flex bg-[#5087D0] font-fredoka items-center px-[10%] py-[12px] text-yellow-500">
-            <Link to="../" className="flex-1 font-bold text-[36px]">StoryTale</Link>
-            <button className="border-[4px] px-[16px] py-[8px] border-[#3e6aa4] hover:bg-[#3e6aa4] hover:border-[#3e6aa4] transition-all duration-200 rounded-[12px]">
+        <div className="flex bg-[#5087D0] font-fredoka items-center px-[10%] py-[12px] text-yellow-500 tracking-widest">
+            <Link to="../" className="flex-1 font-bold text-[36px] drop-shadow-lg">StoryTale</Link>
+            <button className="px-[34px] py-[10px] text-black bg-yellow-500 border-yellow-600 border-[4px] hover:bg-yellow-600 transition-all duration-200 rounded-[16px] drop-shadow-lg">
                 <Link to="../Login" className="text-[20px]">Login</Link>
             </button>
         </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ function Input({ label, id, type }) {
     return (
         <>  
             <label for={id} className="ml-[12px] mb-[4px]">{label}</label>
-            <input type={type} id={id}className="mb-[16px] bg-yellow-400 rounded-[24px] outline-0 border-b-2 p-[10px] py-[14px] w-[80%]items-center"></input>
+            <input type={type} id={id}className="mb-[16px] bg-yellow-400 rounded-[24px] outline-0 border-[4px] border-yellow-500 p-[24px] text-[18px] py-[14px] w-[80%]items-center"></input>
         </>
     )
 }
