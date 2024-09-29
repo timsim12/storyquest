02d4a80bfedb762d7eeb0e71a0e5ee15fc3f8f7e
Lexline Johnson <lexlinejj27@gmail.com>
2024-09-28 21:19:47 -0400

    This too oops

--- FRONTEND/src/pages/ParentCorner.js
+++ FRONTEND/src/pages/ParentCorner.js
@@ -13,17 +13,3 @@ function ParentCorner() {
 }
 
 export default ParentCorner;
-import Header from "../components/Header";
-import StarGoals from '../components/StarGoals';
-
-function ParentCorner() {
-    return (
-        <div className="font-fredoka tracking-widest">
-            <Header /> 
-            {/* maybe remove star goals since this is for the parent (or modify it) */}
-            <StarGoals />
-        </div>
-    )
-}
-
-export default ParentCorner;
