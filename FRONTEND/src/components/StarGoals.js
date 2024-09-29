222f08dceec75000f26b85154a2c22b33ea2aa73
tkare <tkareng12336@gmail.com>
2024-09-29 04:50:34 -0400

    Fixed Star goals so that it will display the name of the child depending on the account.

--- FRONTEND/src/components/StarGoals.js
+++ FRONTEND/src/components/StarGoals.js
@@ -1,11 +1,50 @@
 import StarImage from "./StarImage";
 import StarMeter from "./StarMeter";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useState, useEffect } from "react";
+import { getDocumentData } from "../firebase";
 
 function StarGoals() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    const [userDocs, setUserDocs] = useState(null);
+
+
+    useEffect(() => {
+        // Set up an authentication state observer
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                // User is logged in
+                try {
+                    const currUserDocs = await getDocumentData(user.email);
+                    if (currUserDocs) {
+                        setUserDocs(currUserDocs);
+                    }
+                } catch (error) {
+                    console.error("Error fetching document data:", error);
+                }
+            } else {
+                // No user is logged in
+                console.log("No user is logged in");
+            }
+        });
+
+        // Clean up the observer on component unmount
+        return () => unsubscribe();
+    }, [auth]);
+
+    
     return (
         <>
             <div className="items-center bg-white p-[20px] mx-[10%] rounded-[20px] text-[18px] mt-[20px] px-[60px]">
-                <h1 className="font-semibold text-[22px] text-yellow-500">Hi, Tomy!</h1>
+                {userDocs ? (
+                        <>
+                            <h1 className="font-semibold text-[22px] text-yellow-500">Hi, {userDocs.childName}</h1>
+                        </>
+                    ) : (
+                        <p>Loading user data...</p>
+                    )}
                 <div className="flex">
                     <p>Your next reward is:&nbsp;</p>
                     <p className="font-semibold text-[#5087D0] mb-[8px]">5 pieces of candy</p>
