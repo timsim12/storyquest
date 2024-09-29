1b06b139f29ecbb52b77745dc83c9be7dd4f7b15
baibrea <breannacbai@gmail.com>
2024-09-28 22:03:02 -0400

    Modified style of book pages and added books

--- FRONTEND/src/data.js
+++ FRONTEND/src/data.js
@@ -3,27 +3,27 @@ export const books = [
         title: "The Three Little Pigs",
         author: "Multiple authors",
         cover: "three-little-pigs.jpg",
-        content: "Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes. The first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.\nThe third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.\nThe next day, a wolf happened to pass by the lane where the three little pigs lived; and he saw the straw house, and he smelled the pig inside. He thought the pig would make a mighty fine meal and his mouth began to water."
+        content: "Once upon a time there was an old mother pig who had three little pigs and not enough food to feed them. So when they were old enough, she sent them out into the world to seek their fortunes.\n\nThe first little pig was very lazy. He didn't want to work at all and he built his house out of straw. The second little pig worked a little bit harder but he was somewhat lazy too and he built his house out of sticks. Then, they sang and danced and played together the rest of the day.\n\nThe third little pig worked hard all day and built his house with bricks. It was a sturdy house complete with a fine fireplace and chimney. It looked like it could withstand the strongest winds.\n\nThe next day, a wolf happened to pass by the lane where the three little pigs lived; and he saw the straw house, and he smelled the pig inside. He thought the pig would make a mighty fine meal and his mouth began to water.\n\nSo he knocked on the door and said:\n\nLittle pig! Little pig!\nLet me in! Let me in!\n\nBut the little pig saw the wolf's big paws through the keyhole, so he answered back:\n\nNo! No! No!\nNot by the hairs on my chinny chin chin!\n\nSo the wolf showed his teeth and said:\n\nThen I'll huff\nand I'll puff\nand I'll blow your house down!\n\nSo he huffed and he puffed and he blew the house down! The wolf was greedy and he tried to catch both pigs at once, but he was too greedy and got neither! His big jaws clamped down on nothing but air and the two little pigs scrambled away as fast as their little hooves would carry them.\n\nThe wolf chased them down the lane and he almost caught them. But they made it to the brick house and slammed the door closed before the wolf could catch them. The three little pigs they were very frightened, they knew the wolf wanted to eat them. And that was very, very true. The wolf hadn't eaten all day and he had worked up a large appetite chasing the pigs around and now he could smell all three of them inside and he knew that the three little pigs would make a lovely feast.\n\nSo the wolf knocked on the door and said:\n\nLittle pigs! Little pigs!\nLet me in! Let me in!\n\nBut the little pigs saw the wolf's narrow eyes through the keyhole, so they answered back:\n\nNo! No! No!\nNot by the hairs on our chinny chin chins!\n\nSo the wolf showed his teeth and said:\n\nThen I'll huff\nand I'll puff\nand I'll blow your house down!\n\nHe huffed and he puffed. He puffed and he huffed; but he could not blow the house down. At last, he was so out of breath that he couldn't huff and he couldn't puff anymore. So he stopped to rest and thought a bit.\n\nBut this was too much. The wolf danced about with rage and swore he would come down the chimney and eat up the little pig for his supper. But while he was climbing on to the roof the little pig made up a blazing fire and put on a big pot full of water to boil. Then, just as the wolf was coming down the chimney, the little piggy pulled off the lid, and plop! in fell the wolf into the scalding water.\n\nSo the little piggy put on the cover again, boiled the wolf up, and the three little pigs ate him for supper."
     }, {
-        title: "The Three Little Pigs",
-        author: "Multiple authors",
-        cover: "three-little-pigs.jpg",
-        content: "hi"
+        title: "I Like to Read!",
+        author: "Letta Machoga",
+        cover: "i-like-to-read.jpg",
+        content: "I like to read.\nWho can I read to?\n\nMy sister is asleep.\nWho can I read to?\n\nMy mother and grandmother are busy.\nWho can I read to?\n\nMy father and grandfather are busy.\nWho can I read to?\n\nI can read to myself!"
     }, {
-        title: "The Three Little Pigs",
-        author: "Multiple authors",
-        cover: "three-little-pigs.jpg",
-        content: "hi"
+        title: "Feelings",
+        author: "Clare Verbeek, Thembani Dladla, Zanele Buthelezi",
+        cover: "feelings.jpg",
+        content: "My heart feels a lot of things.\n\nI feel happy when my granny tells us stories in the evening.\n\nI feel silly when I play with my friend.\n\nI feel bad when my dad says he does not have money.\n\nI feel loved when my mom gives me a hug."
     }, {
-        title: "The Three Little Pigs",
-        author: "Multiple authors",
-        cover: "three-little-pigs.jpg",
-        content: "hi"
+        title: "Chicken and Millipede",
+        author: "Winny Asara",
+        cover: "chicken-and-millipede.jpg",
+        content: "Chicken and Millipede were friends. But they were always competing with each other. One day they decided to play football to see who the best player was.\n\nThey went to the football field and started their game. Chicken was fast, but Millipede was faster. Chicken kicked far, but Millipede kicked further. Chicken started to feel grumpy.\n\nThey decided to play a penalty shoot-out. First Millipede was goal keeper. Chicken scored only one goal. Then it was the chicken’s turn to defend the goal.\n\nMillipede kicked the ball and scored. Millipede dribbled the ball and scored. Millipede headed the ball and scored. Millipede scored five goals.\n\nChicken was furious that she lost. She was a very sore loser. Millipede started laughing because his friend was making such a fuss.\n\nChicken was so angry that she opened her beak wide and swallowed the millipede.\n\nAs Chicken was walking home, she met Mother Millipede. Mother Millipede asked, “Have you seen my child?” Chicken didn’t say anything. Mother Millipede was worried.\n\nThen Mother Millipede heard a tiny voice. “Help me mom!” cried the voice. Mother Millipede looked around and listened carefully. The voice came from inside the chicken.\n\nMother Millipede shouted, “Use your special power my child!” Millipedes can make a bad smell and a terrible taste. Chicken began to feel ill.\n\nChicken burped. Then she swallowed and spat. Then she sneezed and coughed. And coughed. The millipede was disgusting!\n\nChicken coughed until she coughed out the millipede that was in her stomach. Mother Millipede and her child crawled up a tree to hide.\n\nFrom that time, chickens and millipedes were enemies."
     }, {
-        title: "The Three Little Pigs",
-        author: "Multiple authors",
-        cover: "three-little-pigs.jpg",
-        content: "hi"
+        title: "The Hungry Crocodile",
+        author: "Christian G.",
+        cover: "the-hungry-crocodile.jpg",
+        content: "Once there was a very hungry crocodile.\n\nHe searched for food slowly and quietly. And then...\n\nPOW!!!\n\nThe crocodile strikes!\n\nAfter that he is no longer hungry, and he is happy."
     }, {
         title: "The Three Little Pigs",
         author: "Multiple authors",
--- FRONTEND/src/pages/BookView.js
+++ FRONTEND/src/pages/BookView.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 function BookView({title, author, cover, content}) {
@@ -7,20 +8,31 @@ function BookView({title, author, cover, content}) {
             console.log(selection);
         }
     }
+
+    const addLineBreak = (string) =>
+        string.split('\n').map((subStr) => {
+          return (
+            <>
+              {subStr}
+              <br />
+            </>
+          );
+        });
     
     return (
-        <div className="font-fredoka tracking-widest">
+        <>
             <Header />
-            <div className="bg-white mt-[20px] mx-[10%] rounded-[20px] p-[20px] px-[60px]">
-                <div className="capitalize">
+            <div className="bg-white mt-[20px] mx-[20%] rounded-[20px] p-[20px] font-fredoka tracking-widest">
+                <Link to="/Books" className="bg-red-400 p-[14px] rounded-[8px] mt-[14px] hover:bg-red-300 transition-all duration-200">Back</Link>
+                <div className="capitalize ml-[20%]">
                     <h1 className="text-[40px] font-bold text-left text-[#5087D0] drop-shadow-lg">{title}</h1>
                     <h1 className="mb-[24px]">{`by: ${author}`}</h1>
                 </div>
                 <img src={`/covers/${cover}`} alt={title} className="rounded-[20px] w-[60%] max-w-[400px] mx-auto mb-[24px]"/>
-                <body className="text-[20px]" onMouseUp={selectedText}>{content}</body>
+                <p className="text-[20px] mx-[10%]" onMouseUp={selectedText}>{addLineBreak(content)}</p>
                 <h1 className="mt-[48px] mb-[24px] text-[40px] font-bold text-left text-[#5087D0] drop-shadow-lg">Reading Quiz</h1>
             </div>
-        </div>
+        </>
     )
 }
 
