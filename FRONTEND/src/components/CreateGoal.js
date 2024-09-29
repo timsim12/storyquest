60881fbf4cdb9c0a6d9da6e9b6e9dd3f21e94394
Lexline Johnson <lexlinejj27@gmail.com>
2024-09-29 06:37:17 -0400

    Spacing (help)

--- FRONTEND/src/components/CreateGoal.js
+++ FRONTEND/src/components/CreateGoal.js
@@ -12,7 +12,7 @@ function CreateGoal({isPresented}) {
     if (isPresented) {
         return (
             <>
-                <form className="flex flex-col" onSubmit={addGoal}>
+                <form className="flex flex-col mt-[24px] mb-[-16px]" onSubmit={addGoal}>
                     <Input type="goalText" id="goalText" label="Goal" value={goalText} onChange={(e) => setGoalText(e.target.value)}/>
                     <Input type="stars" id="stars" label="Stars" value={stars} onChange={(e) => setStars(e.target.value)}/>
                     {/* <input type="submit" value="Log in" className="p-[14px] hover:cursor-pointer rounded-[20px] bg-[#9DEEBD] mb-[30px] mt-[20px] hover:bg-[#6eb98c] transition-all duration-200 text-[18px]" /> */}
